1cbcf8a39535813cfa2f0524dd9f76390c5559e1
Craig Ridgwell <craig@tenderloins.com.au>
2013-07-22 19:04:00 +1000

    added movie app

---
+++ 09_models_active_record/code_alongs/craig_movie_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CraigMovieApp::Application.config.secret_key_base = 'bd074b86c2cdc822dbbc18603704244b0f21cfa397850d8ac3cc35d082020be92e3252e888d61edcc1efcaa004749529974f50d626b8bdcceeeb21dd0f333faa'
